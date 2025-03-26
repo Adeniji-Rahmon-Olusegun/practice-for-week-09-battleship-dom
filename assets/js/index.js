3bcb22bfafd6537d99306569330a7e1564dba32b
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2025-03-26 19:28:18 +0100

    battle ship game

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,3 +7,61 @@ let board = new Board(); // creates a new game board
 console.log(board.grid);
 
 // Your code here
+window.addEventListener("DOMContentLoaded", event => {
+    let scoreBoard = document.getElementById("scores");
+
+    const boardGrid = document.getElementById("board-grid");
+
+    //creating the list items
+    const createListItems = function () {
+        for (let row = 0; row < board.numRows; row++) {
+            for (let col = 0; col < board.numCols; col++) {
+                const element = document.createElement("li");
+                // if (board.grid[row][col]) {
+                //     element.innerHTML = board.grid[row][col];
+                // }
+                
+                element.setAttribute("data-row", row);
+                element.setAttribute("data-col", col);
+                //element.setAttribute("data-mark", "unmarked");
+                boardGrid.append(element);
+            }
+        }
+    }
+
+    createListItems();
+    
+
+    let gridManipulator = function (event) {
+        let row = event.target.dataset.row;
+        let col = event.target.dataset.col;
+        //let markStatus = event.target.dataset.mark;
+
+        console.log(row, col);
+        
+        let gridValue = board.makeHit(row, col);
+
+        if (gridValue) {
+            event.target.style.backgroundColor = "green";
+            event.target.innerHTML = gridValue;
+            scoreBoard.innerHTML = `Lets kick the game rollong 👍: ${board.numRemaining}`;
+        } else {
+            event.target.style.backgroundColor = "red";
+        }
+
+        if (board.numRemaining === 0) {
+            boardGrid.classList.add("disabled");
+        }
+    }
+
+    boardGrid.addEventListener("click", gridManipulator);
+
+    let reset = document.getElementById("reset-button");
+
+    reset.addEventListener("click", (event) => {
+        boardGrid.innerHTML = "";
+        board.numRemaining = 17;
+        createListItems();
+        boardGrid.classList.remove("disabled");
+    });
+});
